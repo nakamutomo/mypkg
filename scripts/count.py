89327f8f6d8d1f1659fcd449cfd27ef3c6d2f05f
nakatsukasa <tomaty214@gmail.com>
2021-12-29 05:37:55 +0000

    Update

--- scripts/count.py
+++ scripts/count.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python3
+
 import rospy
 from std_msgs.msg import Int32
 
-rospy.init_node('count')                                # ノード名「count」に設定
-pub = rospy.Publisher('count_up', Int32, queue_size=1)  # パブリッシャ「count_up」を作成
-rate = rospy.Rate(10)                                   # 10Hzで実行
+rospy.init_node('count')
+pub = rospy.Publisher('count_up', Int32, queue_size=1)
+rate = rospy.Rate(10)
 n = 0
 while not rospy.is_shutdown():
         n += 1
